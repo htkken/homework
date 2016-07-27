cc3c0cb591a9b52e70cebf4b91e858403b331ae3
Ken <Ken>
2016-07-27 23:34:30 +0800

    change

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+//    $("td").click(function(){
+//         $(this).hide();
+//    $("td[colspan='2']").click(function(){$("td[colspan!='2']").fadeToggle(1000);
+//    $("td").click(function(){console.log($(this).parent().text()
+//                                    );});
+//                                         
+//                                          $("td").click(function(){$(this).text("good");});
+//              
+//     
+    
+    $("td").click(function(){
+$(this).remove();
+});
+    });
+    
+    
+})
